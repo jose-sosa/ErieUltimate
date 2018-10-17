4dd7185376b191216af6d31e260ce9927b4f9ae0
Travon Campbell <travonlcampbell@gmail.com>
2018-10-17 17:37:31 -0400

    Did header

--- TMS/src/app/app.module.ts
+++ TMS/src/app/app.module.ts
@@ -2,12 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './header/header.component'; 
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { SideComponent } from './side/side.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent, 
+    MainComponent,
+    FooterComponent,
+    SideComponent
   ],
   imports: [
     BrowserModule
